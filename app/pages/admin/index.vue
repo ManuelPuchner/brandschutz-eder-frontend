fb0776050c2c19b7f9d417cd1a3fdb3d50fccfb4
ManuelPuchner <manuelpuchner@icloud.com>
2026-04-07 18:22:58 +0200

    fixed nav on /admin

--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 // Define page meta
 definePageMeta({
-  layout: 'admin'
-})
-
+  layout: "admin",
+});
 </script>
 
-
 <template>
-  dashboard home
-  todo
-</template>
+  <UDashboardPanel>
+    <template #header>
+      <UDashboardNavbar title="Kunden">
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+      </UDashboardNavbar>
+    </template>
 
+    <template #body>
+      <p>Willkommen im Admin Dashboard. Hier können Sie Ihre Anwendung verwalten und wichtige Metriken anzeigen.</p>
+    </template>
+  </UDashboardPanel>
+</template>
